d31ba943aa95cf5d882eefed5b1447261d6f0943
stanley <stanleygomesdasilva@gmail.com>
2026-08-27 12:24:51 -0300

    chore: change default vite port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -75,6 +75,14 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    port: 3000,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
